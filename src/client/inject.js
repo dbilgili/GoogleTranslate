c33c25340288f7216cc8088aa7d5f098fb458934
Doğacan Bilgili <dbilgili@sabanciuniv.edu>
2021-03-31 21:22:25 +0200

    Update inject.js

--- src/client/inject.js
+++ src/client/inject.js
@@ -3,6 +3,13 @@ iframeDOM = {
   deletePageContents: () => {
     const header = document.getElementsByTagName('header')[0];
     header.parentNode.removeChild(header);
+    document.body.style.transform = 'translateY(-56px)'
+
+    const bottomNav = document.getElementsByTagName('nav')[1]
+    bottomNav.remove()
+
+    const sendFeedbackText = document.querySelectorAll('[aria-haspopup]')[0]
+    sendFeedbackText.remove()
 
     const giveFeedback = document.querySelector('.feedback-link');
     giveFeedback.parentNode.removeChild(giveFeedback);
@@ -13,11 +20,8 @@ iframeDOM = {
     const frame = document.querySelector('.frame');
     frame.style = 'height:100vh;';
   },
-  focusTextArea: () => {
-    document.querySelector('#source').focus();
-  },
   clearTextArea: () => {
-    document.querySelector('#source').value = '';
+    document.getElementsByTagName('button')[12].click();
   },
   getURL: () => window.location.hash,
   setURL: (arg) => {
